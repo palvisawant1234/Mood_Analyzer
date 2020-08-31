ff3f5197844fc1b23f78ddca23ae6a67fb5b6f13
sawantpalvi1234 <sawantpalvi087@gmail>
2020-08-31 10:06:57 +0530

    Added:TC 1.1 sad message return after sad mood

--- MoodAnalyzerTest.java
+++ MoodAnalyzerTest.java
@@ -1,3 +1,12 @@
-public class MoodAnalyserTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MoodAnalyserTest {
+    private MoodAnalyser analyser;
+
+    @Test
+    public void givenMood_WhenSadMessage_ShouldReturnSad() {
+        analyser = new MoodAnalyser();
+        Assert.assertEquals("Sad",analyser.analyseMood("I Am In Sad Mood"));
+    }
 }
